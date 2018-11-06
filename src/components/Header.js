8e401a2a85e14996e705a815e6c8340d2c3918ae
Natthawat Singkala <natthawat@hospital-os.com>
2018-11-06 10:10:59 +0700

    6/10 V.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,10 +13,10 @@ class Header extends Component {
           <Link to="/" className="ml1 no-underline black">
             new
           </Link>
+          <div className="ml1">|</div>
           <Link to="/top" className="ml1 no-underline black">
             top
           </Link>
-          <div className="ml1">|</div>
           {authToken && (
             <div className="flex">
               <div className="ml1">|</div>
